3c3e5cf65be27c9eeeef41688003c8ea0ed097ea
Federico Boschini <federico@nearit.com>
2019-02-07 00:20:35 +0100

    refactor(sample): update

--- nearit_sdk/example/lib/main.dart
+++ nearit_sdk/example/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:nearit_sdk/nearit_sdk.dart';
+import 'package:nearit_sdk/objects/Content.dart';
 import 'package:nearit_sdk/objects/Coupon.dart';
+import 'package:nearit_sdk/objects/CustomJSON.dart';
+import 'package:nearit_sdk/objects/NearItFeedback.dart';
+import 'package:nearit_sdk/objects/HistoryItem.dart';
+import 'package:nearit_sdk/objects/SimpleNotification.dart';
 
 void main() => runApp(new MyApp());
 
@@ -14,6 +19,8 @@ class _MyAppState extends State<MyApp> {
   String _profileId = 'Unknown';
 
   final myController = new TextEditingController();
+  final keyController = new TextEditingController();
+  final valueController = new TextEditingController();
 
   @override
   void dispose() {
@@ -63,11 +70,42 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
-  getInbox() {
-    NearitSdk
-        .getInbox()
-        .then((inbox) => print("Inbox: " + inbox.toString()))
-        .catchError((error) => print(error));
+  setUserData() {
+    NearitSdk.setUserData("chiave", valueController.text);
+  }
+
+  getNotificationHistory() async {
+    List<HistoryItem> inbox;
+    try {
+      inbox = await NearitSdk.getNotificationHistory();
+    } on PlatformException {
+      print("Failed parsing inbox");
+    }
+
+    for (HistoryItem item in inbox) {
+      if (item.reaction is SimpleNotification) {
+        SimpleNotification simple = item.reaction;
+        print(simple.notificationMessage);
+      } else if (item.reaction is CustomJSON) {
+        CustomJSON customJSON = item.reaction;
+        print(customJSON.notificationMessage);
+        print(customJSON.content);
+      } else if (item.reaction is Content) {
+        Content content = item.reaction;
+        print(content.notificationMessage);
+        print("Title:" + content.title);
+        print("Content:" + content.content);
+        print("Image:" + content.image?.fullSize);
+        print(content.cta?.label);
+        print(content.cta?.url);
+      } else if (item.reaction is NearItFeedback) {
+        NearItFeedback feedback = item.reaction;
+        print(feedback.notificationMessage);
+        print(feedback.question);
+        print(feedback.trackingInfo.metadata);
+        NearitSdk.sendTracking(feedback.trackingInfo, "event");
+      }
+    }
   }
 
   getCoupons() async {
@@ -78,11 +116,40 @@ class _MyAppState extends State<MyApp> {
       print("Failed parsing coupons");
     }
 
-    print(couponList[0].image.fullSize);
+    if (couponList.isNotEmpty) {
+      Coupon coupon = couponList[0];
+      print("Title: "+ coupon.title);
+      print("Description: "+ coupon.description);
+      print("Value: "+ coupon.value);
+      print("ClaimedAt: "+ coupon.claimedAt);
+      var exp = coupon.expiresAt ?? "";
+      print("ExpiresAt: "+ exp);
+      var redeemableFrom = coupon.redeemableFrom ?? "";
+      print("ReedemableFrom: "+ redeemableFrom);
+      var redeemedAt = coupon.redeemedAt ?? "";
+      print("ReedemableAt: "+ redeemedAt);
+      print("Image: "+ coupon.image.fullSize);
+    }
   }
 
   triggerInAppEvent() async {
     NearitSdk.triggerInAppEvent("in_app_event_test");
+  }
+
+  triggerContent() async {
+    NearitSdk.triggerInAppEvent("content");
+  }
+
+  triggerFeedback() async {
+    NearitSdk.triggerInAppEvent("feedback");
+  }
+
+  triggerCustomJson() async {
+    NearitSdk.triggerInAppEvent("json");
+  }
+
+  triggerCoupon() async {
+    NearitSdk.triggerInAppEvent("coupon");
   }
 
   @override
@@ -123,7 +190,7 @@ class _MyAppState extends State<MyApp> {
                   new Padding(
                     padding: new EdgeInsets.only(top: 5.0),
                     child: new RaisedButton(
-                        child: const Text('GET INBOX'), onPressed: getInbox),
+                        child: const Text('GET INBOX'), onPressed: getNotificationHistory),
                   ),
                   new Padding(
                     padding: new EdgeInsets.only(top: 5.0),
@@ -154,11 +221,65 @@ class _MyAppState extends State<MyApp> {
                     ),
                   ),
                   new Padding(
+                    padding: new EdgeInsets.only(top: 5.0),
+                    child: new Flex(
+                      direction: Axis.horizontal,
+                      children: [
+                        new Flexible(
+                          child: new TextField(
+                            controller: keyController,
+                            decoration: new InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'key'),
+                          ),
+                        ),
+                        new Flexible(
+                          child: new TextField(
+                            controller: valueController,
+                            decoration: new InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'value'),
+                          ),
+                        ),
+                        new Padding(
+                          padding: new EdgeInsets.only(left: 5.0),
+                          child: new RaisedButton(
+                              child: const Text('SET USER DATA'),
+                              onPressed: setUserData),
+                        )
+                      ],
+                    ),
+                  ),
+                  new Padding(
                       padding: new EdgeInsets.only(top: 5.0),
                       child: new RaisedButton(
-                          child: const Text('TRIGGER EVENT'),
+                          child: const Text('TRIGGER SIMPLE'),
                           onPressed: triggerInAppEvent),
-                  )
+                  ),
+                  new Padding(
+                      padding: new EdgeInsets.only(top: 5.0),
+                      child: new RaisedButton(
+                          child: const Text('TRIGGER CONTENT'),
+                          onPressed: triggerContent),
+                  ),
+                  new Padding(
+                      padding: new EdgeInsets.only(top: 5.0),
+                      child: new RaisedButton(
+                          child: const Text('TRIGGER FEEDBACK'),
+                          onPressed: triggerFeedback),
+                  ),
+                  new Padding(
+                      padding: new EdgeInsets.only(top: 5.0),
+                      child: new RaisedButton(
+                          child: const Text('TRIGGER CUSTOMJSON'),
+                          onPressed: triggerCustomJson),
+                  ),
+                  new Padding(
+                      padding: new EdgeInsets.only(top: 5.0),
+                      child: new RaisedButton(
+                          child: const Text('TRIGGER COUPON'),
+                          onPressed: triggerCoupon),
+                  ),
                 ],
               )),
             ],
